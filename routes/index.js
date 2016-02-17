b3ca964441eb6408045aa8fcd66623a3ed3b28f2
Darren Parkinson <dparkinson@natilik.com>
2016-02-17 17:33:34 +0000

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.post('/api/v1/webhook',
+  function (req, res, next) {
+        console.log(req.body.text);
+        res.end('Authenticated')
+});
+  
+module.exports = router;
